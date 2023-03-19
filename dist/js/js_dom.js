729da0a1a38641f1379c6f5f8c94cd4d76aa9c1d
pketgto388 <pketgto388@gmail.com>
2023-03-19 11:44:41 +0800

    20230319

---
+++ dist/js/js_dom.js
@@ -0,0 +1,26 @@
+let uid = document.querySelector('#uid');
+
+let students = document.querySelector('.student');
+
+console.log('------ uid ------')
+console.log(uid);
+console.log('------ students ------')
+console.log(students);
+
+let m_uid = document.querySelectorAll('#uid');
+
+let m_students = document.querySelectorAll('.student');
+
+
+console.log('------ m_uid ------')
+console.log(m_uid);
+console.log('------ m_students ------')
+console.log(m_students);
+
+m_uid.forEach(u => {
+    console.log(u);
+})
+
+m_students.forEach(s => {
+    console.log(s);
+})
