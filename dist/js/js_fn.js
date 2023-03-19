f375f91d4938d4f5c26e1e26240c773dde88f391
pketgto388 <pketgto388@gmail.com>
2023-03-19 11:16:02 +0800

    20230319

--- dist/js/js_fn.js
+++ dist/js/js_fn.js
@@ -1,6 +1,10 @@
-function add(num1, num2) {
+// function add(num1, num2) {
+//     return num1 + num2;
+//     console.log('Hello');
+// }
+
+const add = function (num1, num2) {
     return num1 + num2;
-    console.log('Hello');
 }
 
 let calc = add(1, 10);
