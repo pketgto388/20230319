ba0e9e5ead119e56ca8c456035f68f42cf052771
pketgto388 <pketgto388@gmail.com>
2023-03-19 14:34:21 +0800

    20230319

--- dist/js/js_dom.js
+++ dist/js/js_dom.js
@@ -37,3 +37,30 @@ const login = function () {
 
     acc.value = 'ffff';
 }
+
+
+uid.style.backgroundColor = 'red';
+
+console.log(uid.classList);
+
+// uid.classList.add('secondary');
+// uid.classList.remove('main');
+
+
+
+const myContains = function (dom, className) {
+    let cname = dom.className;
+    let cnameArr = cname.split(' ');
+
+    cnameArr.forEach(n => {
+        if (n == className) {
+            console.log(`${className} in dom element. use function`);
+        }
+    })
+}
+
+if (uid.classList.contains('main')) {
+    console.log('main in uid element. use contains');
+}
+
+myContains(uid, 'main');
